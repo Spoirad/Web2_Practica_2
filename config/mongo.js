9511508fd48738673e1358820997b16f362837f4
Spoirad <angellopezpaparella2004@gmail.com>
2025-05-01 22:59:17 +0200

    .

--- config/mongo.js
+++ config/mongo.js
@@ -7,10 +7,7 @@ const dbConnect = async () => {
     : process.env.DB_URI;
 
   try {
-    await mongoose.connect(uri, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    });
+    await mongoose.connect(uri);
     console.log(`✅ Conectado a la base de datos ${process.env.NODE_ENV === "test" ? "de testing" : "principal"}`);
   } catch (err) {
     console.error("❌ Error conectando a la base de datos:", err);
